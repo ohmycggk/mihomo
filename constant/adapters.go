5eebed514a141b0d4313efe5d70a417d1425d11d
MiaM1ku <61079068+MiaM1ku@users.noreply.github.com>
2026-09-11 09:41:09 +0800

    feat: add EasyTier outbound (#3194)

--- constant/adapters.go
+++ constant/adapters.go
@@ -53,6 +53,7 @@ const (
 	OpenVPN
 	Tailscale
 	ZeroTier
+	EasyTier
 	GostRelay
 	Nowhere
 )
@@ -238,6 +239,8 @@ func (at AdapterType) String() string {
 		return "Tailscale"
 	case ZeroTier:
 		return "ZeroTier"
+	case EasyTier:
+		return "EasyTier"
 	case GostRelay:
 		return "GostRelay"
 	case Nowhere:
